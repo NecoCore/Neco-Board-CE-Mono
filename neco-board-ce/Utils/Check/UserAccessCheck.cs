2e0f551868a97553bbb35ff42fa6d1dc7ea7c779
CreperBerig <rcreper135@gmail.com>
2026-05-18 14:36:42 +0300

    edit: updated using repositoryes in UserAccessCheck

--- neco-board-ce/Utils/Check/UserAccessCheck.cs
+++ neco-board-ce/Utils/Check/UserAccessCheck.cs
@@ -34,9 +34,13 @@ namespace neco_board_ce.Utils.Check
         public async Task<ChekResult> HasAccessToColumn(string userId, string columnId)
         {
             var column = await _columnsRepository.GetById(columnId);
-            if(column is null) return new ChekResult { Result = false, Message = "Column not found" };
+            if(!column.Success)
+            {
+                return new ChekResult { Result = false, Message = column.Message };
+            }
+            if(column.Data is null) return new ChekResult { Result = false, Message = "Column not found" };
 
-            var projectId = column.ProjectId;
+            var projectId = column.Data.ProjectId;
             var userInProject = _userProjectRoleRepository.GetByUserAndProject(userId, projectId);
             if (userInProject is null) return new ChekResult { Result = false, Message = "User dosen't access have in project" };
 
@@ -46,9 +50,10 @@ namespace neco_board_ce.Utils.Check
         public async Task<ChekResult> HasAccessToTask(string userId, string taskId)
         {
             var projectId = await _taskRepository.GetProjectById(taskId);
-            if (projectId is null) return new ChekResult { Result = false, Message = "Task not found" };
+            if(!projectId.Success) return new ChekResult { Result = false, Message = projectId.Message };
+            if (projectId.Data is null) return new ChekResult { Result = false, Message = "Task not found" };
 
-            var userInProject = _userProjectRoleRepository.GetByUserAndProject(userId, projectId);
+            var userInProject = _userProjectRoleRepository.GetByUserAndProject(userId, projectId.Data);
             if (userInProject is null) return new ChekResult { Result = false, Message = "User dosen't have access in project" };
 
             return new ChekResult { Result = true };
@@ -56,8 +61,9 @@ namespace neco_board_ce.Utils.Check
 
         public async Task<ChekResult> HasAccessToProject(string userId, string projectId)
         {
-            var userInProject = _userProjectRoleRepository.GetByUserAndProject(userId, projectId);
-            if (userInProject is null) return new ChekResult { Result = false, Message = "User dosen't have access in project" };
+            var userInProject = await _userProjectRoleRepository.GetByUserAndProject(userId, projectId);
+            if(userInProject.Success) return new ChekResult { Result = false, Message = userInProject.Message };
+            if (userInProject.Data is null) return new ChekResult { Result = false, Message = "User dosen't have access in project" };
 
             return new ChekResult { Result = true };
         }
