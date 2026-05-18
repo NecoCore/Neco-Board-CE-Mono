c15f3eb039161fb928f37810694bfadbc922971a
CreperBerig <rcreper135@gmail.com>
2026-05-18 12:44:23 +0300

    edit: added Repository result class and updated ICRUDRepository

--- neco-board-ce/Interfaces/ICRUDRepository.cs
+++ neco-board-ce/Interfaces/ICRUDRepository.cs
@@ -1,11 +1,13 @@
-﻿namespace neco_board_ce.Interfaces
+﻿using neco_board_ce.Models.Results;
+
+namespace neco_board_ce.Interfaces
 {
     public interface ICRUDRepository<T>
     {
-        Task<List<T>> GetAll();
-        Task<T?> GetById(string id);
-        Task<bool> Create(T entity);
-        Task<bool> Update(string id, T entity);
-        Task<bool> Delete(string id);
+        Task<RepositoryResult<List<T>>> GetAll();
+        Task<RepositoryResult<T?>> GetById(string id);
+        Task<RepositoryResult<bool>> Create(T entity);
+        Task<RepositoryResult<bool>> Update(string id, T entity);
+        Task<RepositoryResult<bool>> Delete(string id);
     }
 }
---
+++ neco-board-ce/Models/Results/RepositoryResult.cs
@@ -0,0 +1,9 @@
+﻿namespace neco_board_ce.Models.Results
+{
+    public class RepositoryResult<T>
+    {
+        public bool Success { get; set; }
+        public T? Data { get; set; }
+        public string? Message { get; set; }
+    }
+}
