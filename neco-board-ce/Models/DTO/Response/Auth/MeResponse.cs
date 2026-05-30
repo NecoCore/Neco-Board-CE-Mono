36ba7ed5dc9023106fa53be07040b9996b95e3c6
CreperBerig <rcreper135@gmail.com>
2026-05-30 23:52:04 +0300

    add: create Me Response

---
+++ neco-board-ce/Models/DTO/Response/Auth/MeResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace neco_board_ce.Models.DTO.Response.Auth
+{
+    public class MeResponse
+    {
+        public string Id { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string Login { get; set; } = string.Empty;
+        public string Role { get; set; } = string.Empty;
+        public string? Avatar { get; set; } = string.Empty;
+    }
+}
