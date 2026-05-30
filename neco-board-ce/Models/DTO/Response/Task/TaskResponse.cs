d8996e5ad3d52613636bbc647988e1ad29c885ee
CreperBerig <rcreper135@gmail.com>
2026-05-30 18:35:31 +0300

    add: Task response

---
+++ neco-board-ce/Models/DTO/Response/Task/TaskResponse.cs
@@ -0,0 +1,15 @@
+﻿using neco_board_ce.Models.Enums;
+
+namespace neco_board_ce.Models.DTO.Response.Task
+{
+    public class TaskResponse
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public TaskPriority Priority { get; set; }
+        public ColumnTaskStatus Status { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string ColumnId { get; set; }
+    }
+}
