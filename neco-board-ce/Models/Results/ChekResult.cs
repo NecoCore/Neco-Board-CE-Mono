42c82c260cec2a4f41cea8416e71187b8d46c340
CreperBerig <rcreper135@gmail.com>
2026-05-30 18:36:37 +0300

    etid: add project id for answer

--- neco-board-ce/Models/Results/ChekResult.cs
+++ neco-board-ce/Models/Results/ChekResult.cs
@@ -4,5 +4,6 @@
     {
         public bool Result { get; set; }
         public string? Message { get; set; } = string.Empty;
+        public string? ProjectId { get; set; } = string.Empty;
     }
 }
