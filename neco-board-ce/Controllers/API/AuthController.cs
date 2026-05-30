b213427561d38288cfaf06cd3b9797741dfb437f
CreperBerig <rcreper135@gmail.com>
2026-05-30 23:52:33 +0300

    feat: add returns information for swagger documentation

--- neco-board-ce/Controllers/API/AuthController.cs
+++ neco-board-ce/Controllers/API/AuthController.cs
@@ -1,13 +1,16 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using System.Security.Claims;
-using neco_board_ce.Services.Authentication;
 using neco_board_ce.Models.DTO.Request;
+using neco_board_ce.Models.DTO.Response.Auth;
+using neco_board_ce.Models.DTO.Response.Massages;
+using neco_board_ce.Services.Authentication;
+using System.Security.Claims;
 
 namespace neco_board_ce.Controllers.API
 {
     [ApiController]
     [Route("api/[controller]")]
+    [Tags("Authification")]
     public class AuthController : ControllerBase
     {
         private readonly AuthService _authService;
@@ -21,27 +24,33 @@ namespace neco_board_ce.Controllers.API
             _config = configuration;
         }
 
-        [HttpPost("login")]
+        [HttpPost("login", Name = "Login")]
+        [ProducesResponseType(typeof(RefreshResponse), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ErrorMessageResponse), StatusCodes.Status401Unauthorized)]
         public async Task<IActionResult> Login([FromBody] LoginRequest dto)
         {
             var result = await _authService.LoginAsync(dto);
-            if (!result.Success) return Unauthorized(new { result.Error });
+            if (!result.Success) return Unauthorized(new ErrorMessageResponse { Message = result.Error ?? "unknown error" });
 
             SetRefreshTokenCookie(result.RefreshToken!);
-            return Ok(new { result.AccessToken });
+            return Ok(new RefreshResponse { AccessToken = result.AccessToken! });
         }
 
-        [HttpPost("register")]
+        [HttpPost("register", Name = "Register")]
         [Authorize(Roles = "ADMIN,OWNER")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ErrorMessageResponse), StatusCodes.Status401Unauthorized)]
         public async Task<IActionResult> Register([FromBody] RegisterRequest dto)
         {
             var result = await _authService.RegisterAsync(dto);
-            if (!result.Success) return BadRequest(new { result.Error });
+            if (!result.Success) return BadRequest(new ErrorMessageResponse { Message = result.Error ?? "unknown error" });
 
             return Ok();
         }
 
-        [HttpPost("refresh")]
+        [HttpPost("refresh", Name = "Refresh")]
+        [ProducesResponseType(typeof(RefreshResponse), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ErrorMessageResponse), StatusCodes.Status401Unauthorized)]
         public async Task<IActionResult> Refresh()
         {
             var refreshToken = Request.Cookies["refreshToken"];
@@ -55,14 +64,16 @@ namespace neco_board_ce.Controllers.API
             if (!result.Success)
             {
                 _logger.LogError("Error refreshing token: {error}", result.Error);
-                return Unauthorized(new { result.Error });
+                return Unauthorized(new ErrorMessageResponse { Message = result.Error ?? "unknown error" });
             }
 
             SetRefreshTokenCookie(result.RefreshToken!);
-            return Ok(new { result.AccessToken });
+            return Ok(new RefreshResponse { AccessToken = result.AccessToken! });
         }
 
-        [HttpPost("logout")]
+        [HttpPost("logout", Name = "Logout")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         public async Task<IActionResult> Logout()
         {
             var refreshToken = Request.Cookies["refreshToken"];
@@ -79,14 +90,16 @@ namespace neco_board_ce.Controllers.API
             return Ok();
         }
 
-        [HttpGet("me")]
+        [HttpGet("me", Name = "GetMeInfo")]
         [Authorize]
-        public IActionResult Me() => Ok(new
+        [ProducesResponseType(typeof(MeResponse), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        public IActionResult Me() => Ok(new MeResponse
         {
-            Id = User.FindFirstValue(ClaimTypes.NameIdentifier),
-            Login = User.FindFirstValue(ClaimTypes.Name),
-            Role = User.FindFirstValue(ClaimTypes.Role),
-            Name = User.FindFirstValue("name"),
+            Id = User.FindFirstValue(ClaimTypes.NameIdentifier)!,
+            Login = User.FindFirstValue(ClaimTypes.Name)!,
+            Role = User.FindFirstValue(ClaimTypes.Role)!,
+            Name = User.FindFirstValue("name")!,
             Avatar = User.FindFirstValue("avatar")
         });
 
