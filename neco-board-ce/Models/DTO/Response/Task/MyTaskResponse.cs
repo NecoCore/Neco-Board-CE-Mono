409a1f70f1eeca5b8a53deeb551d76b701885cdc
CreperBerig <rcreper135@gmail.com>
2026-05-31 15:59:07 +0300

    feat: add response for user tasks

---
+++ neco-board-ce/Models/DTO/Response/Task/MyTaskResponse.cs
@@ -0,0 +1,15 @@
+﻿using neco_board_ce.Models.Enums;
+
+namespace neco_board_ce.Models.DTO.Response.Task
+{
+    public class MyTaskResponse
+    {
+        public string Id { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public ColumnTaskStatus Status { get; set; }
+        public TaskPriority Priority { get; set; }
+        public DateTime CreateAt { get; set; }
+        public string ColumnId { get; set; } = string.Empty;
+    }
+}
