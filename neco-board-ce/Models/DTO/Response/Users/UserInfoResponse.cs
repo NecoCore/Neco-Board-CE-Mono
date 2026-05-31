46a0beb9944b6aff16021a43f8795ac33e0943b2
CreperBerig <rcreper135@gmail.com>
2026-05-31 15:58:49 +0300

    feat: add response for user

---
+++ neco-board-ce/Models/DTO/Response/Users/UserInfoResponse.cs
@@ -0,0 +1,21 @@
+﻿using neco_board_ce.Models.Entity;
+using neco_board_ce.Models.Enums;
+
+namespace neco_board_ce.Models.DTO.Response.Users
+{
+    public class UserInfoResponse
+    {
+        public string Id { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string? Avatar { get; set; }
+        public WorkspaceRoles Role { get; set; }
+
+        public UserInfoResponse(Account account) 
+        {
+            Id = account.Id;
+            Name = account.Name;
+            Avatar = account.Avatar;
+            Role = account.Role;
+        }
+    }
+}
