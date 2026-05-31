b580d6ff7f6263d1e99cf350f452f75cae6f0642
CreperBerig <rcreper135@gmail.com>
2026-05-31 14:48:03 +0300

    feat: add response for column item

---
+++ neco-board-ce/Models/DTO/Response/Column/ColumnItemResponse.cs
@@ -0,0 +1,18 @@
+﻿namespace neco_board_ce.Models.DTO.Response.Column
+{
+    public class ColumnItemResponse
+    {
+        public string Id { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public int Queue { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        public ColumnItemResponse(neco_board_ce.Models.Entity.Column column) 
+        {
+            Id = column.Id;
+            Name = column.Name;
+            Queue = column.Queue;
+            CreatedAt = column.CreatedAt;
+        }
+    }
+}
