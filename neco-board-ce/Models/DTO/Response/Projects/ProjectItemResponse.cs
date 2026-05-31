cf5cb8d29b9aedb2be16658e969c594d619671b2
CreperBerig <rcreper135@gmail.com>
2026-05-31 11:40:28 +0300

    feat: created little response for project item

---
+++ neco-board-ce/Models/DTO/Response/Projects/ProjectItemResponse.cs
@@ -0,0 +1,16 @@
+﻿using neco_board_ce.Models.Entity;
+
+namespace neco_board_ce.Models.DTO.Response.Projects
+{
+    public class ProjectItemResponse
+    {
+        public string Id { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+
+        public ProjectItemResponse(Project project)
+        {
+            Id = project.Id;
+            Name = project.Name;
+        }
+    }
+}
