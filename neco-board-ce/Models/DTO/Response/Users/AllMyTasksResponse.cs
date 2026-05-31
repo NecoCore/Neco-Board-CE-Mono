04ded5984b86d300f397db997c46ef173afcafc6
CreperBerig <rcreper135@gmail.com>
2026-05-31 15:59:33 +0300

    feat: add response user tasks with projects

---
+++ neco-board-ce/Models/DTO/Response/Users/AllMyTasksResponse.cs
@@ -0,0 +1,11 @@
+﻿using neco_board_ce.Models.DTO.Response.Task;
+
+namespace neco_board_ce.Models.DTO.Response.Users
+{
+    public class AllMyTasksResponse
+    {
+        public string ProjectId { get; set; } = string.Empty;
+        public string ProjectName { get; set; } = string.Empty;
+        public List<MyTaskResponse> Tasks = new List<MyTaskResponse>();
+    }
+}
