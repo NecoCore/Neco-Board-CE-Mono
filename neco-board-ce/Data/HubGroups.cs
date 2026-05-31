00d8d18405f0d7c704cd9c503cc59d847c047cc7
CreperBerig <rcreper135@gmail.com>
2026-05-31 22:00:11 +0300

    add: add static class for hub groups

---
+++ neco-board-ce/Data/HubGroups.cs
@@ -0,0 +1,10 @@
+﻿namespace neco_board_ce.Data
+{
+    public static class HubGroups
+    {
+        public static string Project(string id) => $"project:{id}";
+        public static string Task(string id) => $"task:{id}";
+        public static string All = "all";
+        public static string Admins = "AdminsGroup";
+    }
+}
