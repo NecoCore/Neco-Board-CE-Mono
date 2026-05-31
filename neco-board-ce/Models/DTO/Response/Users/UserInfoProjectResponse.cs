7ffcc35e03089a034a507afa20313e3c6ca65cc1
CreperBerig <rcreper135@gmail.com>
2026-05-31 16:39:32 +0300

    feat: add user project info response

---
+++ neco-board-ce/Models/DTO/Response/Users/UserInfoProjectResponse.cs
@@ -0,0 +1,21 @@
+﻿using neco_board_ce.Models.Entity;
+using neco_board_ce.Models.Enums;
+
+namespace neco_board_ce.Models.DTO.Response.Users
+{
+    public class UserInfoProjectResponse
+    {
+        public string Id { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string? Avatar { get; set; }
+        public ProjectRole Role { get; set; }
+
+        public UserInfoProjectResponse(UserProjectRole account)
+        {
+            Id = account.User.Id;
+            Name = account.User.Name;
+            Avatar = account.User.Avatar;
+            Role = account.Role;
+        }
+    }
+}
