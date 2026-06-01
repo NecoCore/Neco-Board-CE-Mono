e92e1cb3688a2133ecf9f4b9d0d3f8b09c57fc86
CreperBerig <rcreper135@gmail.com>
2026-06-01 14:56:31 +0300

    add: created nofiticator fo controllers

---
+++ neco-board-ce/Interfaces/IRealtimeNotifier.cs
@@ -0,0 +1,40 @@
+﻿using neco_board_ce.Models.Enums;
+
+namespace neco_board_ce.Interfaces
+{
+    public interface IRealtimeNotifier
+    {
+        #region Projects
+        Task ProjectCreated();
+        Task ProjectUpdated(string projectId, string projectName);
+        Task ProjectDeleted(string projectId);
+        #endregion
+
+        #region Users in project
+        Task ProjectAddUser(string projectId, string userId);
+        Task ProjectUpdateUser(string projectId, string userId, ProjectRole newRole);
+        Task ProjectRemoveUser(string projectId, string userId);
+        #endregion
+
+        #region Columns
+        Task ColumnCreated(string projectId);
+        Task ColumnUpdated(string projectId, string columnId, string columnName);
+        Task ColumnOrderUpdated(string projectId);
+        Task ColumnDelete(string projectId, string columnId);
+        #endregion
+
+        #region Tasks
+        Task TaskCreated(string projectId, string columnId);
+        Task TaskUpdated(string projectId, string taskId);
+        Task TaskColumnUpdated(string projectId, string oldColumnId, string newColumnId);
+        Task TaskStatusUpdated(string projectId, string taskId, string columnId, ColumnTaskStatus newStatus);
+        Task TaskPriorityUpdated(string projectId, string taskId, string columnId, TaskPriority newPriority);
+        Task TaskDelete(string projectId, string columnId, string taskId);
+        #endregion
+
+        #region Users in task
+        Task TaskAddUser(string taskId);
+        Task TaskRemoveUser(string taskId, string userId);
+        #endregion
+    }
+}
