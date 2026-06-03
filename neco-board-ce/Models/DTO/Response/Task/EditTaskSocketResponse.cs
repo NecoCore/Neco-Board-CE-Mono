c787bf83ed57487f412b132a4ff24e5e65d60259
CreperBerig <rcreper135@gmail.com>
2026-06-03 11:03:55 +0300

    feat: removed old sokets response

--- neco-board-ce/Models/DTO/Response/Task/EditTaskSocketResponse.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace neco_board_ce.Models.DTO.Response.Task
-{
-    public class EditTaskSocketResponse
-    {
-        public string TaskId { get; set; }
-        public string ColumnId { get; set; }
-    }
-}
--- neco-board-ce/Models/DTO/Response/Task/NewTaskColumnSocketRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace neco_board_ce.Models.DTO.Response.Task
-{
-    public class NewTaskColumnSocketRequest
-    {
-        public string OldColumnId { get; set; } = string.Empty;
-        public string NewColumnId { get; set;} = string.Empty;
-    }
-}
