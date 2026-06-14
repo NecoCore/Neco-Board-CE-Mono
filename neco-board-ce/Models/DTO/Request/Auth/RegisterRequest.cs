4a847f1f0ec93dfc7e4e1a1f36fc45bd842b76f1
CreperBerig <rcreper135@gmail.com>
2026-06-14 06:59:59 +0300

    feat: using FluentValidation for RegisterRequest

--- neco-board-ce/Models/DTO/Request/Auth/RegisterRequest.cs
+++ neco-board-ce/Models/DTO/Request/Auth/RegisterRequest.cs
@@ -1,25 +1,10 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace neco_board_ce.Models.DTO.Request.Auth
+﻿namespace neco_board_ce.Models.DTO.Request.Auth
 {
     public class RegisterRequest
     {
-        [Required]
-        [StringLength(100, MinimumLength = 3, ErrorMessage = "Name must be between 3 and 100 characters.")]
         public string Name { get; set; } = string.Empty;
-
-        [Required]
-        [RegularExpression(@"^[a-zA-Z0-9_]+$", ErrorMessage = "Login can only contain letters, numbers, and underscores.")]
-        [StringLength(50, MinimumLength = 3, ErrorMessage = "Login must be between 3 and 50 characters.")]
         public string Login { get; set; } = string.Empty;
-
-        [Required]
-        [StringLength(100, MinimumLength = 6, ErrorMessage = "Password must be between 6 and 100 characters.")]
-        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$", ErrorMessage = "Password must contain at least one uppercase letter, one lowercase letter, and one number.")]
         public string Password { get; set; } = string.Empty;
-
-        [Required]
-        [Compare(nameof(Password), ErrorMessage = "Passwords do not match.")]
         public string ConfirmPassword { get; set; } = string.Empty;
     }
 }
---
+++ neco-board-ce/Models/DTO/Validations/RegisterRequestValidator.cs
@@ -0,0 +1,30 @@
+﻿using FluentValidation;
+using neco_board_ce.Models.DTO.Request.Auth;
+
+namespace neco_board_ce.Models.DTO.Validations
+{
+    public class RegisterRequestValidator : AbstractValidator<RegisterRequest>
+    {
+        public RegisterRequestValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("Name is required.")
+                .Length(3, 100).WithMessage("Name must be between 3 and 100 characters.");
+
+            RuleFor(x => x.Login)
+                .NotEmpty().WithMessage("Login is required.")
+                .Length(3, 50).WithMessage("Login must be between 3 and 50 characters.")
+                .Matches(@"^[a-zA-Z0-9_]+$").WithMessage("Login can only contain letters, numbers, and underscores.");
+
+            RuleFor(x => x.Password)
+                .NotEmpty().WithMessage("Password is required.")
+                .Length(6, 100).WithMessage("Password must be between 6 and 100 characters.")
+                .Matches(@"[A-Z]").WithMessage("Password must contain at least one uppercase letter.")
+                .Matches(@"[a-z]").WithMessage("Password must contain at least one lowercase letter.")
+                .Matches(@"\d").WithMessage("Password  must contain at least one number");
+
+            RuleFor(x => x.ConfirmPassword)
+                .Equal(x => x.Password).WithMessage("Passwords do not match");
+        }
+    }
+}
