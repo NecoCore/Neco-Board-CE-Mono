ab79f4d96e408a721c3830dc189d8f139b61a33f
CreperBerig <rcreper135@gmail.com>
2026-06-14 07:47:24 +0300

    feat: used FluentValidation for UserProjectRequest

--- neco-board-ce/Models/DTO/Request/Projects/UserProjectRequest.cs
+++ neco-board-ce/Models/DTO/Request/Projects/UserProjectRequest.cs
@@ -1,14 +1,10 @@
 ﻿using neco_board_ce.Models.Enums;
-using System.ComponentModel.DataAnnotations;
 
 namespace neco_board_ce.Models.DTO.Request.Projects
 {
     public class UserProjectRequest
     {
-        [Required]
         public string Id { get; set; }
-
-        [Required]
         public ProjectRole Role { get; set; } = ProjectRole.USER;
     }
 }
---
+++ neco-board-ce/Models/DTO/Validations/UserProjectRequestValidator.cs
@@ -0,0 +1,17 @@
+using FluentValidation;
+using neco_board_ce.Models.DTO.Request.Projects;
+
+namespace neco_board_ce.Models.DTO.Validations
+{
+    public class UserProjectRequestValidator : AbstractValidator<UserProjectRequest>
+    {
+        public UserProjectRequestValidator()
+        {
+            RuleFor(x => x.Id)
+                .NotEmpty().WithMessage("User ID is required.");
+
+            RuleFor(x => x.Role)
+                .IsInEnum().WithMessage("Invalid project role.");
+        }
+    }
+}
