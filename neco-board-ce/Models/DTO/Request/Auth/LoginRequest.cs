4f73b95dcbbad90b79a2297364d0077e49c5517f
CreperBerig <rcreper135@gmail.com>
2026-06-14 07:38:55 +0300

    fix: add get and set for filds in LoginRequest

--- neco-board-ce/Models/DTO/Request/Auth/LoginRequest.cs
+++ neco-board-ce/Models/DTO/Request/Auth/LoginRequest.cs
@@ -2,7 +2,7 @@
 {
     public class LoginRequest
     {
-        public string Login = string.Empty;
-        public string Password = string.Empty;
+        public string Login { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
     };
 }
