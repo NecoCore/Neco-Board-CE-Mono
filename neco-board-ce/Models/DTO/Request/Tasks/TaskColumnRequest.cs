e69cf0b8d5dde5ae94c9c995e811503773c9142c
CreperBerig <rcreper135@gmail.com>
2026-06-14 08:28:07 +0300

    feat: used FluentValidation for ColumnRequest

--- neco-board-ce/Models/DTO/Request/Tasks/TaskColumnRequest.cs
+++ neco-board-ce/Models/DTO/Request/Tasks/TaskColumnRequest.cs
@@ -1,24 +1,14 @@
 ﻿using neco_board_ce.Models.Enums;
-using System.ComponentModel.DataAnnotations;
 
 namespace neco_board_ce.Models.DTO.Request.Tasks
 {
     public class TaskColumnRequest
     {
-        [Required]
-        [MaxLength(250)]
-        public string Name { get; set; }
-
-        [Required]
-        public string ColumnId { get; set; }
-
-        [MaxLength(1000)]
+        public string Name { get; set; } = string.Empty;
+        public string ColumnId { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
-
         public string Text { get; set; } = string.Empty;
-
         public TaskPriority Priority { get; set; } = TaskPriority.LOW;
-
         public ColumnTaskStatus Status { get; set; } = ColumnTaskStatus.NOT_STARTED;
     }
 }
---
+++ neco-board-ce/Models/DTO/Validations/TaskColumnRequestValidator.cs
@@ -0,0 +1,27 @@
+using FluentValidation;
+using neco_board_ce.Models.DTO.Request.Tasks;
+
+namespace neco_board_ce.Models.DTO.Validations
+{
+    public class TaskColumnRequestValidator : AbstractValidator<TaskColumnRequest>
+    {
+        public TaskColumnRequestValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("Task name is required.")
+                .MaximumLength(250).WithMessage("Task name cannot exceed 250 characters.");
+
+            RuleFor(x => x.ColumnId)
+                .NotEmpty().WithMessage("Column ID is required.");
+
+            RuleFor(x => x.Description)
+                .MaximumLength(1000).WithMessage("Description cannot exceed 1000 characters.");
+
+            RuleFor(x => x.Priority)
+                .IsInEnum().WithMessage("Invalid task priority.");
+
+            RuleFor(x => x.Status)
+                .IsInEnum().WithMessage("Invalid task status.");
+        }
+    }
+}
