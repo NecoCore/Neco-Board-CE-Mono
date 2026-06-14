49c540c538d1e5096840c2f541509247ae11ddf2
CreperBerig <rcreper135@gmail.com>
2026-06-14 13:48:06 +0300

    edit: update log entry response structure

--- neco-board-ce/Models/DTO/Response/Logs/LogItemResponse.cs
+++ neco-board-ce/Models/DTO/Response/Logs/LogItemResponse.cs
@@ -5,16 +5,16 @@ namespace neco_board_ce.Models.DTO.Response.Logs
 {
     public class LogItemResponse
     {
-        public string Id { get; set; } = string.Empty;
+        public Guid Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public string? Description { get; set; }
         public LogType LogType { get; set; }
         public DateTime CreatedAt { get; set; }
         
-        public string UserId { get; set; } = string.Empty;
+        public Guid UserId { get; set; }
         public string UserName { get; set; } = string.Empty;
         
-        public string? ProjectId { get; set; }
+        public Guid? ProjectId { get; set; }
         public string? ProjectName { get; set; }
 
         public LogItemResponse() { }
