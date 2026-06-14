fa9e0723c860cff9ea1699e49f937d7f43d9a257
CreperBerig <rcreper135@gmail.com>
2026-06-14 13:48:40 +0300

    edit: update project list item response

--- neco-board-ce/Models/DTO/Response/Projects/ProjectItemResponse.cs
+++ neco-board-ce/Models/DTO/Response/Projects/ProjectItemResponse.cs
@@ -4,13 +4,15 @@ namespace neco_board_ce.Models.DTO.Response.Projects
 {
     public class ProjectItemResponse
     {
-        public string Id { get; set; } = string.Empty;
+        public Guid Id { get; set; }
         public string Name { get; set; } = string.Empty;
+        public bool IsArchived { get; set; }
 
         public ProjectItemResponse(Project project)
         {
             Id = project.Id;
             Name = project.Name;
+            IsArchived = project.IsArchived;
         }
     }
 }
