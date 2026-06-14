187d8bb11a1ecdadd41f87863263e6b540eb083e
CreperBerig <rcreper135@gmail.com>
2026-06-14 13:37:45 +0300

    edit: refine generic repository interface signatures

--- neco-board-ce/Interfaces/ICRUDRepository.cs
+++ neco-board-ce/Interfaces/ICRUDRepository.cs
@@ -5,9 +5,9 @@ namespace neco_board_ce.Interfaces
     public interface ICRUDRepository<T>
     {
         Task<RepositoryResult<List<T>>> GetAll();
-        Task<RepositoryResult<T?>> GetById(string id);
+        Task<RepositoryResult<T?>> GetById(Guid id);
         Task<RepositoryResult<bool>> Create(T entity);
-        Task<RepositoryResult<bool>> Update(string id, T entity);
-        Task<RepositoryResult<bool>> Delete(string id);
+        Task<RepositoryResult<bool>> Update(Guid id, T entity);
+        Task<RepositoryResult<bool>> Delete(Guid id);
     }
 }
