fddb9404e0cb14c276a1f149f4b779cc36de7668
CreperBerig <rcreper135@gmail.com>
2026-06-14 09:41:07 +0300

    feat: add LogFor for

---
+++ neco-board-ce/Models/Enums/LogFor.cs
@@ -0,0 +1,11 @@
+﻿namespace neco_board_ce.Models.Enums
+{
+    public enum LogFor
+    {
+        USER = 0,
+        FILE = 1,
+        PROJECT = 2,
+        COLUMN = 3,
+        TASK = 4,
+    }
+}
